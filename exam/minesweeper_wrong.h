60f9786dfd15c97026378df631cca723141a5d3a
alzobnin <alzobnin@users.noreply.github.com>
2015-12-14 19:28:16 +0300

    main function

--- exam/minesweeper_wrong.h
+++ exam/minesweeper_wrong.h
@@ -64,21 +64,3 @@ std::ostream& operator << (std::ostream& out, const Minesweeper& ms) {
     return out;
 }
 
-int main() {
-    size_t m, n;
-    std::cin >> m >> n;
-    Minesweeper ms(m, n);
-
-    int minesCount;
-    std::cin >> minesCount;
-    while (minesCount-->0) {
-        size_t x, y;
-        std::cin >> x >> y;
-        ms.SetMine(x, y);
-    }
-
-    ms.CheckForMinesAround();
-
-    std::cout << ms;
-}
-
