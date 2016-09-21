ec46983b796a39fbe9df4b4adf124bc0a9329d3c
alzobnin <alzobnin@users.noreply.github.com>
2016-09-21 11:03:04 +0400

    ngrams

---
+++ ngrams2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+#include <random>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    int n = atoi(argv[1]);
+
+    map<string, vector<int>> freqs;
+    char c;
+    string context;
+    while (cin.get(c)) {
+        if (c < 0 || c > 127)
+            continue;
+        if (context.size() == n) {
+            if (freqs.find(context) == freqs.end())
+                freqs[context].resize(128);
+            ++freqs[context][c];
+        }
+        context.push_back(c);
+        if (context.size() > n)
+            context.erase(context.begin());
+    }
+
+    context = "Hello";
+    context.resize(n);
+    cout << context;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    for (int i = 0; i != 100; ++i) {
+        const auto& context_freqs = freqs[context];
+        discrete_distribution<> distr(context_freqs.begin(), context_freqs.end());
+        char c = distr(gen);
+        cout.put(c);
+        context.push_back(c);
+        context.erase(context.begin());
+    }
+    cout << "\n";
+}
