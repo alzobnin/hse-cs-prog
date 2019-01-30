8274789cd70cd8529bca56d87ede5ee0615b3137
alzobnin <alzobnin@users.noreply.github.com>
2019-01-30 16:59:04 +0300

    Multibyte encoding support

--- ngrams2.cpp
+++ ngrams2.cpp
@@ -13,25 +13,23 @@ int main(int argc, char * argv[]) {
     char c;
     string context;
     while (cin.get(c)) {
-        if (c < 0 || c > 127)
-            continue;
         if (context.size() == n) {
             if (freqs.find(context) == freqs.end())
-                freqs[context].resize(128);
-            ++freqs[context][c];
+                freqs[context].resize(256);
+            ++freqs[context][static_cast<unsigned char>(c)];
         }
         context.push_back(c);
         if (context.size() > n)
             context.erase(context.begin());
     }
 
-    context = "Hello";
+    context = "Россия";
     context.resize(n);
     cout << context;
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    for (int i = 0; i != 100; ++i) {
+    for (int i = 0; i != 1000; ++i) {
         const auto& context_freqs = freqs[context];
         discrete_distribution<> distr(context_freqs.begin(), context_freqs.end());
         char c = distr(gen);
